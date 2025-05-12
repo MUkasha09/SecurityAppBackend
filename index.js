ba742c6f0c2e3f6496f41aec0cc92d00a53918d1
MuhammadSufian98 <sufianhassan98@gmail.com>
2025-05-12 23:31:50 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const http = require("http");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+const ESP32_CAM_URL = "http://192.168.10.120";
+
+app.use(cors());
+app.use(express.json()); // For parsing JSON bodies
+
+app.use(
+  "/camera",
+  createProxyMiddleware({
+    target: ESP32_CAM_URL,
+    changeOrigin: true,
+  })
+);
+
+app.post("/sensorTrigger", (req, res) => {
+  const { status } = req.body;
+  if (status === "motion") {
+    io.emit("alarmTriggered", { message: "🚨 Motion detected!" });
+  }
+  res.send({ success: true });
+});
+
+// Start server
+server.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
